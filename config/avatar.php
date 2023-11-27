bc3412f45581b9385c15e638db8137584eeb78cb
Paula Raielle <63271436+paularaielle@users.noreply.github.com>
2023-11-27 09:33:49 -0300

    Update avatar.php

--- config/avatar.php
+++ config/avatar.php
@@ -4,31 +4,16 @@ return [
 
     'frango' => [
         'img' => 'img/avatar/frango.png',
-        'pontos' => '100'
-    ],
-
-    'vaca' => [
-        'img' => 'img/avatar/vaca.png',
-        'pontos' => '100'
+        'pontos' => '0'
     ],
 
     'ovelha' => [
         'img' => 'img/avatar/ovelha.png',
-        'pontos' => '200'
+        'pontos' => '0'
     ],
 
-    'raposa' => [
-        'img' => 'img/avatar/raposa.png',
-        'pontos' => '300'
-    ],
-
-    'tigre' => [
-        'img' => 'img/avatar/tigre.png',
-        'pontos' => '400'
-    ],
-
-    'panda-vermelho' => [
-        'img' => 'img/avatar/panda-vermelho.png',
+    'encanador' => [
+        'img' => 'img/avatar/encanador.png',
         'pontos' => '500'
     ],
 
@@ -37,33 +22,48 @@ return [
         'pontos' => '500'
     ],
 
-    'leao' => [
-        'img' => 'img/avatar/leao.png',
-        'pontos' => '700'
-    ],
-
-    'encanador' => [
-        'img' => 'img/avatar/encanador.png',
-        'pontos' => '800'
-    ],
-
-    'wolverine' => [
-        'img' => 'img/avatar/wolverine.png',
-        'pontos' => '850'
+    'vaca' => [
+        'img' => 'img/avatar/vaca.png',
+        'pontos' => '1000'
     ],
 
     'flash' => [
         'img' => 'img/avatar/flash.png',
-        'pontos' => '900'
+        'pontos' => '1000'
+    ],
+
+    'raposa' => [
+        'img' => 'img/avatar/raposa.png',
+        'pontos' => '1500'
     ],
 
     'batman' => [
         'img' => 'img/avatar/batman.png',
-        'pontos' => '950'
+        'pontos' => '1500'
+    ],
+
+     'wolverine' => [
+        'img' => 'img/avatar/wolverine.png',
+        'pontos' => '2000'
+    ],
+
+    'tigre' => [
+        'img' => 'img/avatar/tigre.png',
+        'pontos' => '2000'
+    ],
+
+    'panda-vermelho' => [
+        'img' => 'img/avatar/panda-vermelho.png',
+        'pontos' => '2500'
+    ],
+
+    'leao' => [
+        'img' => 'img/avatar/leao.png',
+        'pontos' => '2500'
     ],
 
     'thor' => [
         'img' => 'img/avatar/thor.png',
-        'pontos' => '950'
+        'pontos' => '2500'
     ],
 ];
